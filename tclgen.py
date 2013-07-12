84ec048a682364ee1572abc946d6944d067f62b0
Fabrizio Spada <fabrizio.spada89.com>
2013-07-12 14:48:38 +0200

    sistemato tclgen

--- tclgen.py
+++ tclgen.py
@@ -6,29 +6,27 @@ def gen_tcl_core(file_source,func_name,clock_period):
 
 	tcl_directives = open("cores/hls/core_"+func_name+"/solution1/directives.tcl","w")
 
-	tcl_directives.write("set_directive_interface -mode ap_none "+ func_name + " a\n")
-	tcl_directives.write("set_directive_interface -mode ap_none "+ func_name + " b\n")
-	tcl_directives.write("set_directive_interface -mode ap_ctrl_hs -register "+ func_name + " return\n")
-
-	tcl_directives.write('set_directive_resource -core AXI4LiteS -metadata "-bus_bundle slv0" '+ func_name +' a\n')
-	tcl_directives.write('set_directive_resource -core AXI4LiteS -metadata "-bus_bundle slv0" '+ func_name +' b\n')
-	tcl_directives.write('set_directive_resource -core AXI4LiteS -metadata "-bus_bundle slv0" '+ func_name +' return\n')
+	tcl_directives.write("set_directive_interface -mode ap_none "+ func_name + " a\n" +
+						 "set_directive_interface -mode ap_none "+ func_name + " b\n" +
+						 "set_directive_interface -mode ap_ctrl_hs -register "+ func_name + " return\n" +
+						 'set_directive_resource -core AXI4LiteS -metadata "-bus_bundle slv0" '+ func_name +' a\n' +
+						 'set_directive_resource -core AXI4LiteS -metadata "-bus_bundle slv0" '+ func_name +' b\n' +
+						 'set_directive_resource -core AXI4LiteS -metadata "-bus_bundle slv0" '+ func_name +' return\n')
 
 	tcl_directives.close()
 
 	tcl_script = open("cores/hls/core_"+func_name+"/solution1/script.tcl","w")
 
-	tcl_script.write("open_project core_"+func_name+"\n")
-	tcl_script.write("set_top "+func_name+"\n")
-	tcl_script.write("add_files ../src/"+file_source+"\n")
-	tcl_script.write("open_solution solution1\n")
-	tcl_script.write("set_part {xc7z020clg484-1}\n")
-	tcl_script.write("create_clock -period "+clock_period+"\n")
-
-	tcl_script.write('source "core_'+func_name+'/solution1/directives.tcl"\n')
-	tcl_script.write("csynth_design\n")
-	tcl_script.write("export_design\n")
-	tcl_script.write("exit\n")
+	tcl_script.write("open_project core_"+func_name+"\n" +
+					 "set_top "+func_name+"\n" +
+					 "add_files ../src/"+file_source+"\n" +
+					 "open_solution solution1\n" +
+					 "set_part {xc7z020clg484-1}\n" +
+					 "create_clock -period "+clock_period+"\n" +
+					 'source "core_'+func_name+'/solution1/directives.tcl"\n' +
+					 "csynth_design\n" +
+					 "export_design\n" +
+					 "exit\n")
 
 	tcl_script.close()
 
